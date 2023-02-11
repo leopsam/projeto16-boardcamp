a900db706151f8dc72dadda0d967c6d0cd811a3e
Leonardo Sampaio <leonardo.lps@outlook.com.br>
2023-02-11 16:32:29 -0300

    Update cliente.model.js

--- src/schemas/cliente.model.js
+++ src/schemas/cliente.model.js
@@ -1,8 +1,8 @@
 import joi from "joi";
 
 export const clienteSchema = joi.object({
-  name: joi.string(),
-  phone: joi.required(),
+  name: joi.string().required(),
+  phone: joi.string().min(10).max(11).required(),
   cpf: joi.string().required(),
-  birthday: joi.required(),
+  birthday: joi.date().required(),
 });
