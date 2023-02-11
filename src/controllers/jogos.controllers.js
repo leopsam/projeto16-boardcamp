7a3cdd14c1970ce66abab3363cc18a80ffde7612
Leonardo Sampaio <leonardo.lps@outlook.com.br>
2023-02-11 18:42:02 -0300

    Update jogos.controllers.js

--- src/controllers/jogos.controllers.js
+++ src/controllers/jogos.controllers.js
@@ -17,6 +17,24 @@ export async function criarJogo(req, res) {
 
   console.log(name, image, stockTotal, pricePerDay)  
 
+
+
+
+  if (stockTotal < 0 || pricePerDay < 0) return res.sendStatus(400)
+  //if (isNaN(Number(cpf))) return res.sendStatus(400)
+
+  const nameGame = await db.query("SELECT name FROM games")
+  const arrayGames = nameGame.rows
+  arrayGames.map((g) => {
+    if (g.name === name) return res.sendStatus(409)
+  })
+
+
+
+
+
+
+
   try {
     const jogo = await db.query(`INSERT INTO games (name, image, "stockTotal", "pricePerDay") VALUES ($1, $2, $3, $4);`, [name, image, stockTotal, pricePerDay])
     
