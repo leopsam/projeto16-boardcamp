87bdadbd4506fd41c938b11ff9c30bafcd0a010d
Leonardo Sampaio <leonardo.lps@outlook.com.br>
2023-02-11 16:41:44 -0300

    Update clientes.controllers.js

--- src/controllers/clientes.controllers.js
+++ src/controllers/clientes.controllers.js
@@ -15,26 +15,11 @@ export async function criarCliente(req, res) {
   console.log("criar cliente") //tirar depois
   const { name, phone, cpf, birthday } = req.body
 
-  console.log(name, phone, cpf, birthday) //tirar depois
-
   console.log(Number(cpf))
 
   if (cpf.length != 11) return res.sendStatus(400)
   if (isNaN(Number(cpf))) return res.sendStatus(400)
 
-
-/*
-  const arrayCpf = cpf.split('')
-  console.log(arrayCpf)
-
-  for(let i = 0; arrayCpf.length < i; i++){
-    console.log(arrayCpf[i])
-    if (arrayCpf[i] != 1 || arrayCpf[i] != "2" || arrayCpf[i] != "3" || arrayCpf[i] != "4" || arrayCpf[i] != "5" || arrayCpf[i] != "6" || arrayCpf[i] != "7" || arrayCpf[i] != "8" || arrayCpf[i] != "9" || arrayCpf[i] != "0"){
-      console.log(arrayCpf[i])
-      return res.sendStatus(401)
-    }
-  }
-*/
   const namesClientes = await db.query("SELECT cpf FROM customers")
   const arrayNames = namesClientes.rows
   arrayNames.map((c) => {
@@ -58,9 +43,9 @@ export async function buscarClientePorId(req, res) {
   try {
     const cliente = await db.query(`SELECT * FROM customers WHERE id = $1;`, [id]);
 
-    if (cliente) return res.sendStatus(404)
+    if (!cliente) return res.sendStatus(404)
 
-    res.send(cliente.rows[0]);
+    res.status(200).send(cliente.rows[0]);
   } catch (error) {
     res.status(500).send(error.message)
   }
