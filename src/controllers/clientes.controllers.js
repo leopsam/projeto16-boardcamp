ca9aebba50bc626800e846dbb86b1cfb787d9bdc
Leonardo Sampaio <leonardo.lps@outlook.com.br>
2023-02-11 14:15:03 -0300

    Update clientes.controllers.js

--- src/controllers/clientes.controllers.js
+++ src/controllers/clientes.controllers.js
@@ -12,23 +12,21 @@ export async function buscarCliente(req, res) {
 }
 
 export async function criarCliente(req, res) {
-  console.log("criar cliente")
+  console.log("criar cliente") //tirar depois
   const { name, phone, cpf, birthday } = req.body
 
-  console.log(name, phone, cpf, birthday)  
+  console.log(name, phone, cpf, birthday) //tirar depois
 
-  try {
-    console.log("criar cliente 2")
-   
+  try {   
     const cliente = await db.query(`
     INSERT INTO customers (name, phone, cpf, birthday)
     VALUES ($1, $2, $3, $4);`
       , [name, phone, cpf, birthday])
 
-    console.log(cliente)
+    console.log(cliente) //tirar depois
     
-    res.send(cliente).status(201)
+    res.sendStatus(201)
   } catch (error) {
-    res.status(400).send(error.message)
+    res.send(error.message).status(400)
   }
 }
