19dd192c5a7d39857a31b0f6d7170235129ff258
Leonardo Sampaio <leonardo.lps@outlook.com.br>
2023-02-11 18:31:56 -0300

    Update jogo.model.js

--- src/schemas/jogo.model.js
+++ src/schemas/jogo.model.js
@@ -3,6 +3,6 @@ import joi from "joi";
 export const jogoSchema = joi.object({
   name: joi.string(),
   image: joi.string(),
-  stockTotal: joi.string(),
-  pricePerDay: joi.string(),
+  stockTotal: joi.required(),
+  pricePerDay: joi.required(),
 });
