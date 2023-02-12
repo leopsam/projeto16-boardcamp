3c48d7858bba45503dac6f33a26f64223aae2664
Leonardo Sampaio <leonardo.lps@outlook.com.br>
2023-02-12 00:53:10 -0300

    Update alugueis.controllers.js

--- src/controllers/alugueis.controllers.js
+++ src/controllers/alugueis.controllers.js
@@ -136,44 +136,24 @@ export async function finalizarAluguelPorId(req, res) {
 
   try {
     const aluguel = await db.query(`SELECT * FROM rentals WHERE id = $1;`, [id]);
-    const date = "2023-2-18"
-
     if (aluguel.rows.length === 0) return res.sendStatus(404) 
     if (aluguel.rows[0].returnDate != null) return res.sendStatus(400) //retirar o comentario depois
 
     const game = await db.query(`SELECT * FROM games WHERE id = $1;`, [aluguel.rows[0].gameId])
-
-
-
     const diaAludado = (aluguel.rows[0].rentDate.getFullYear() + "-" + ((aluguel.rows[0].rentDate.getMonth() + 1)) + "-" + (aluguel.rows[0].rentDate.getDate()))  
     const diffInMs   = new Date(data) - new Date(diaAludado)
     const diffInDays = diffInMs / (1000 * 60 * 60 * 24);   
     let delayFee = game.rows[0].pricePerDay * diffInDays
-    const diasPassados = diffInDays - aluguel.rows[0].daysRented
-    //const diaCerto = diaAludado - aluguel.rows[0].daysRented
-    
-    
+    const diasPassados = diffInDays - aluguel.rows[0].daysRented     
     let time = new Date(diaAludado);
     let entraga = new Date();
     entraga.setDate(time.getDate() + diffInDays);
-    //console.log(entraga)
-
     let timeCerto = new Date(diaAludado);
     let entragaCerto = new Date();
     entragaCerto.setDate(timeCerto.getDate() + aluguel.rows[0].daysRented);
-    //console.log(entragaCerto)
-
     const diaCerto = (entragaCerto.getFullYear() + "-" + ((entragaCerto.getMonth() + 1)) + "-" + (entragaCerto.getDate()))
     const diaEntraga = (entraga.getFullYear() + "-" + ((entraga.getMonth() + 1)) + "-" + (entraga.getDate()))
-
-    console.log(diaAludado)
-    console.log(diaEntraga)
-    console.log(diaCerto)
-
-    console.log(diffInDays)
-    console.log(aluguel.rows[0].daysRented)
-    console.log(diasPassados + " dias passado")
-
+    
     if(diaCerto < diaEntraga){
       delayFee = game.rows[0].pricePerDay * diasPassados
     }
@@ -181,19 +161,8 @@ export async function finalizarAluguelPorId(req, res) {
     if(diasPassados <= 0){
       delayFee = 0
     }
-
-    //console.log(dataFormatadaTwo)    
-    console.log(delayFee)
-    //console.log(dataFormatada)
-    //console.log(game.rows[0].pricePerDay)    
-    //console.log(new Date(date) + new Date(5))   
-    //console.log(game) 
-    //console.log(aluguel.rows[0].returnDate != null)
-
-  
     const aluguelReturno = await db.query(`UPDATE rentals SET "returnDate" = '${data}', "delayFee" = '${delayFee}' WHERE id = $1;`, [id])
 
-    //if (!aluguel) return res.sendStatus(404)
     if (aluguel.rows.length === 0) return res.sendStatus(404) 
     if (!aluguelReturno) return res.sendStatus(404)
 
@@ -202,41 +171,3 @@ export async function finalizarAluguelPorId(req, res) {
     res.status(404).send(error.message)
   }
 }
-
-
-
-
-
-
-/*
-
-
-
-export async function atualizarCliente(req, res) {
-  const { id } = req.params
-  const { name, phone, cpf, birthday } = req.body
-
-  console.log(Number(cpf))
-
-  if (cpf.length != 11) return res.sendStatus(400)
-  if (isNaN(Number(cpf))) return res.sendStatus(400)
-
-  const clienteCpf = await db.query(`SELECT * FROM customers WHERE id = $1;`, [id]);
-  console.log(clienteCpf.rows[0].cpf)
-
-  const cpfsClientes = await db.query("SELECT cpf FROM customers")
-  const arrayCpfs = cpfsClientes.rows
-  arrayCpfs.map((c) => {
-    if (c.cpf === cpf && cpf != clienteCpf.rows[0].cpf) return res.sendStatus(409)
-   })  
-
-  try {
-    const cliente = await db.query(`UPDATE Customers SET name = '${name}', phone = '${phone}', cpf = '${cpf}', birthday = '${birthday}' WHERE id = $1;`, [id]);
-    if (!cliente) return res.sendStatus(404)
-
-    res.status(200).send(cliente.rows[0]);
-  } catch (error) {
-    res.status(500).send(error.message)
-  }
-}
-*/
